8d6f50d758d9325847bf2b22b450e0d2a67272a9
Jordan Luders <jordanluders@gmail.com>
2018-02-27 13:11:35 -0600

    Update bdroid_buildcfg.h

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Tab 2"
+#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Tab 4"
 
 #define BLE_INCLUDED TRUE
 #define BTA_GATT_INCLUDED TRUE
